6960764c1e4e0c80f9349d6dae0bd1cf958d0769
Ahmad Aziz <ahmadaziz0441@gmail.com>
2022-12-16 20:51:46 +0800

    enhance the dark mode theme for the description container

--- components/Common/DataDescription.tsx
+++ components/Common/DataDescription.tsx
@@ -4,14 +4,16 @@ import {IconChevronDown} from "tabler-icons-react-native";
 import Card from "./Card";
 import React, {useState} from "react";
 import {useTailwind} from "tailwind-rn";
+import {useUserStore} from "../../store/zustand";
 
 export default function DataDescription({media}) {
     const tailwind = useTailwind();
     const {width} = useWindowDimensions();
     const [descriptionExpand, setDescriptionExpand] = useState(false);
+    const theme = useUserStore(state => state.theme)
 
     return (
-        <Card styles={'mt-4 bg-zinc-700'}>
+        <Card styles={'mt-4 bg-zinc-100 dark:bg-zinc-700'}>
             {!!media?.description && (
                 <TouchableOpacity
                     style={tailwind(`relative flex flex-row items-center text-white ${descriptionExpand ? 'h-auto' : 'h-[60px]'}`)}
@@ -23,7 +25,7 @@ export default function DataDescription({media}) {
                             source={{html: media?.description}}
                             tagsStyles={{
                                 body: {
-                                    color: '#a1a1aa',
+                                    color: theme === 'dark' ? '#a1a1aa' : '#27272a',
                                 }
                             }}
                         />
